83c15c02aa06aa0f33285fbb68a01c1f1dc179e2
pendulum445 <54065244+pendulum445@users.noreply.github.com>
2023-09-28 16:23:23 +0800

    Initial Commit

---
+++ WeTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeTalk
+//
+//  Created by liaoyunjie on 2023/9/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
