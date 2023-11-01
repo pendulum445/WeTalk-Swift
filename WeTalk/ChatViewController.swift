8a2193f4facd89da9d95d7f2a19171e9ad6f0507
pendulum445 <54065244+pendulum445@users.noreply.github.com>
2023-11-01 16:33:40 +0800

    聊天界面新增自己消息的cell，修复进入聊天界面后消息不滚动到最底部的问题

--- WeTalk/ChatViewController.swift
+++ WeTalk/ChatViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import Dispatch
 
 class ChatViewController : UIViewController, UITableViewDataSource, UITableViewDelegate, NavigationBarViewDelegate {
     
     private var friendInfo: FriendInfo?
+    private var hasScrolledToBottom: Bool = false
     
     // MARK: Life Cycle
     init(friendInfo: FriendInfo) {
@@ -27,12 +29,6 @@ class ChatViewController : UIViewController, UITableViewDataSource, UITableViewD
         self.view.addSubview(self.navigationBarView)
         self.view.addSubview(self.inputBarView)
         self.view.addSubview(self.tableView)
-        self.tableView.reloadData()
-        self.tableView.scrollToRow(at: IndexPath(row: 19, section: 0), at: .bottom, animated: false)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         var statusBarHeight: CGFloat = 0
         if let statusBarManager = UIApplication.shared.windows.first?.windowScene?.statusBarManager {
             statusBarHeight = statusBarManager.statusBarFrame.height
@@ -53,15 +49,32 @@ class ChatViewController : UIViewController, UITableViewDataSource, UITableViewD
         ])
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.hasScrolledToBottom = true
+    }
+    
     // MARK: UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !self.hasScrolledToBottom {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.005) {
+                //                if self.chatData.count > 0 {
+                let indexPath = IndexPath(row: self.tableView.numberOfRows(inSection: 0) - 1, section: 0)
+                self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: false)
+                //                }
+            }
+        }
         return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: FriendMessageCell.self), for: indexPath) as! FriendMessageCell
-        
-        return cell
+        if indexPath.row % 2 == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "FriendMessageCell", for: indexPath) as! FriendMessageCell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SelfMessageCell", for: indexPath) as! SelfMessageCell
+            return cell
+        }
     }
     
     // MARK: UITableViewDelegate
@@ -98,11 +111,12 @@ class ChatViewController : UIViewController, UITableViewDataSource, UITableViewD
         tableView.dataSource = self
         tableView.separatorStyle = .none
         tableView.showsVerticalScrollIndicator = false
-        tableView.rowHeight = 56
-        tableView.estimatedRowHeight = UITableView.automaticDimension
-//        tableView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
-        tableView.register(FriendMessageCell.self, forCellReuseIdentifier: String(describing: FriendMessageCell.self))
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 56
+        //        tableView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
         tableView.backgroundColor = UIColor(red: 233.0/255.0, green: 233.0/255.0, blue: 233.0/255.0, alpha: 1.0)
+        tableView.register(FriendMessageCell.self, forCellReuseIdentifier: "FriendMessageCell")
+        tableView.register(SelfMessageCell.self, forCellReuseIdentifier: "SelfMessageCell")
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
@@ -191,7 +205,7 @@ class FriendMessageCell : UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.contentView.backgroundColor = UIColor(red: 233.0/255.0, green: 233.0/255.0, blue: 233.0/255.0, alpha: 1.0)
-//        self.contentView.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi))
+        //        self.contentView.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi))
         self.contentView.addSubview(self.avatarImageView)
         self.contentView.addSubview(self.cornerImageView)
         self.contentView.addSubview(self.messageTextView)
@@ -240,6 +254,70 @@ class FriendMessageCell : UITableViewCell {
         textView.clipsToBounds = true
         textView.layer.cornerRadius = 4
         textView.backgroundColor = .white
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.font = UIFont(name: "PingFangSC-Regular", size: 17)
+        textView.contentInset = UIEdgeInsets(top: 8, left: 10, bottom: 8, right: 8)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+}
+
+class SelfMessageCell : UITableViewCell {
+    
+    // MARK: Life Cycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentView.backgroundColor = UIColor(red: 233.0/255.0, green: 233.0/255.0, blue: 233.0/255.0, alpha: 1.0)
+        //        self.contentView.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi))
+        self.contentView.addSubview(self.avatarImageView)
+        self.contentView.addSubview(self.cornerImageView)
+        self.contentView.addSubview(self.messageTextView)
+        NSLayoutConstraint.activate([
+            self.avatarImageView.rightAnchor.constraint(equalTo: self.contentView.rightAnchor, constant: -16),
+            self.avatarImageView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.avatarImageView.widthAnchor.constraint(equalToConstant: 40),
+            self.avatarImageView.heightAnchor.constraint(equalToConstant: 40),
+            self.cornerImageView.rightAnchor.constraint(equalTo: self.avatarImageView.leftAnchor, constant: -4),
+            self.cornerImageView.centerYAnchor.constraint(equalTo: self.avatarImageView.centerYAnchor),
+            self.cornerImageView.widthAnchor.constraint(equalToConstant: 26),
+            self.cornerImageView.heightAnchor.constraint(equalToConstant: 22),
+            self.messageTextView.rightAnchor.constraint(equalTo: self.avatarImageView.leftAnchor, constant: -10),
+            self.messageTextView.leftAnchor.constraint(greaterThanOrEqualTo: self.contentView.leftAnchor, constant: 16),
+            self.messageTextView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.messageTextView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -16),
+            self.messageTextView.heightAnchor.constraint(greaterThanOrEqualToConstant: 40),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Custom
+    
+    // MARK: Getter
+    private lazy var avatarImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "default_avatar"))
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 4
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var cornerImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "chat_right_corner"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var messageTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "这是一条测试消息哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈"
+        textView.textColor = .black
+        textView.clipsToBounds = true
+        textView.layer.cornerRadius = 4
+        textView.backgroundColor = UIColor(red: 0.55, green: 0.91, blue: 0.5, alpha: 1)
         textView.isEditable = false
         textView.isScrollEnabled = false
         textView.font = UIFont(name: "PingFangSC-Regular", size: 17)
